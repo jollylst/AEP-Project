9364356318ee502f5e6c50a4a78b43b321f09ff0
Jolly <jollylst@hotmail.com>
2017-04-09 13:39:49 -0700

    test 5

--- src/test/symbol/TestSimplySymbol.java
+++ src/test/symbol/TestSimplySymbol.java
@@ -38,4 +38,18 @@ public class TestSimplySymbol {
         assertTrue(hashSet.contains(ssB));
     }
 
+    @Test
+    public void testHashCodeImperfect() {
+        SimpleSymbol[] ssA = new SimpleSymbol[5];
+        ssA[0] = new SimpleSymbol(10, 20, 50);
+        ssA[1] = new SimpleSymbol(25, 50, 10);
+        ssA[2] = new SimpleSymbol(50, 15, 105);
+        ssA[3] = new SimpleSymbol(60, 20, 20);
+        ssA[4] = new SimpleSymbol(85, 25, 25);
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i = 0; i < 5; i++) {
+            hashSet.add(ssA[i].hashCode());
+        }
+        assertEquals(5, hashSet.size());
+    }
 }
