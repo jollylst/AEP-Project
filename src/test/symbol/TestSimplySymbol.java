1c35dc735bf52ea423de098a323b016d07101648
Jolly <jollylst@hotmail.com>
2017-04-09 01:32:55 -0700

    test 1

--- src/test/symbol/TestSimplySymbol.java
+++ src/test/symbol/TestSimplySymbol.java
@@ -6,15 +6,9 @@ import static org.junit.Assert.*;
  * Created by Jolly on 4/9/17.
  */
 public class TestSimplySymbol {
-    @Test
-    public void testEquals() {
-        SimpleSymbol ooA = new SimpleSymbol(5, 10, 20);
-        SimpleSymbol ooA2 = new SimpleSymbol(5, 10, 20);
-        SimpleSymbol ooB = new SimpleSymbol(50, 50, 50);
-        assertEquals(ooA, ooA2);
-        assertNotEquals(ooA, ooB);
-        assertNotEquals(ooA2, ooB);
-        assertNotEquals(ooA, "ketchup");
+    @Test(expected = IllegalArgumentException.class)
+    public void colorValueBeyondLimitShouldNotBeCreatable() {
+        SimpleSymbol ooA = new SimpleSymbol(5, 10, 265);
     }
 
 }
